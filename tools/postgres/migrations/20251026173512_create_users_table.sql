61268896d9d4cac2a3b2e066481161335a8b3e8d
木村龍太 <kimuraryuuta@kimuraryuutanoMacBook-Air.local>
2025-10-28 02:16:31 +0900

    add: delete migration

--- tools/postgres/migrations/20251026173512_create_users_table.sql
+++ tools/postgres/migrations/20251026173512_create_users_table.sql
@@ -1,6 +1,6 @@
 -- +goose Up
 CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
