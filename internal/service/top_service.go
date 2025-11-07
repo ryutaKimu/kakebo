21b3c9a1452375d1d0ada88ef7b3aacaf3e2d433
木村龍太 <kimuraryuuta@kimuraryuutanoMacBook-Air.local>
2025-11-08 02:55:12 +0900

    use go routine

--- internal/service/top_service.go
+++ internal/service/top_service.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/ryutaKimu/kakebo/internal/repository/top"
 	"github.com/ryutaKimu/kakebo/internal/service/interfaces"
+	"golang.org/x/sync/errgroup"
 )
 
 type TopServiceImpl struct {
@@ -17,32 +18,49 @@ func NewTopService(topRepository top.TopRepository) interfaces.TopService {
 }
 
 func (s *TopServiceImpl) GetMonthlyTotalIncome(ctx context.Context, userId int) (float64, error) {
+	var (
+		fixedIncomeAmount float64
+		subIncomeAmount   float64
+		adjustmentAmount  float64
+	)
 	month := int(time.Now().Month())
-	fixedIncome, err := s.repo.GetSumFixedIncome(ctx, userId, month)
-	if err != nil {
+
+	g, ctx := errgroup.WithContext(ctx)
+
+	g.Go(func() error {
+		fi, err := s.repo.GetSumFixedIncome(ctx, userId, month)
+		if err != nil {
+			return err
+		}
+		fixedIncomeAmount = fi
+		return nil
+	})
+
+	g.Go(func() error {
+		si, err := s.repo.GetSumSubIncome(ctx, userId, month)
+		if err != nil {
+			return err
+		}
+		subIncomeAmount = si
+		return nil
+	})
+
+	g.Go(func() error {
+		adj, err := s.repo.GetSumIncomeAdjustment(ctx, userId, month)
+		if err != nil {
+			return err
+		}
+		adjustmentAmount = adj
+		return nil
+	})
+
+	if err := g.Wait(); err != nil {
 		return 0, err
 	}
 
-	subIncome, err := s.repo.GetSumSubIncome(ctx, userId, month)
-	if err != nil {
-		return 0, err
-	}
-
-	incomeAdjusted, err := s.repo.GetSumIncomeAdjustment(ctx, userId, month)
-	if err != nil {
-		return 0, err
-	}
-
-	costs, err := s.repo.GetSumCost(ctx, userId, month)
-	if err != nil {
-		return 0, err
-	}
-
-	totalAmount := fixedIncome + subIncome
-
-	total := (totalAmount + incomeAdjusted) - costs
-
+	total := (fixedIncomeAmount + subIncomeAmount) + adjustmentAmount
 	return total, nil
+
 }
 
 func (s *TopServiceImpl) GetSumCost(ctx context.Context, userId int) (float64, error) {
