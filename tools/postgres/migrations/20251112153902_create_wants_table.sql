fa09c4aab8d470cdc67ab1072f45649721452627
木村龍太 <kimuraryuuta@kimuraryuutanoMacBook-Air.local>
2025-11-13 02:34:32 +0900

    add: cascade to wants and savings

--- tools/postgres/migrations/20251112153902_create_wants_table.sql
+++ tools/postgres/migrations/20251112153902_create_wants_table.sql
@@ -2,7 +2,7 @@
 -- +goose StatementBegin
 CREATE TABLE wants (
     id BIGSERIAL PRIMARY KEY,
-    user_id BIGINT NOT NULL REFERENCES users (id),
+    user_id BIGINT NOT NULL REFERENCES users (id) ON DELETE CASCADE,
     name VARCHAR(255) NOT NULL, -- 欲しいものの名前
     target_amount NUMERIC(10, 2) NOT NULL, -- 目標金額
     target_date DATE NOT NULL, -- 目標購入日
--- tools/postgres/migrations/20251112161521_create_savings_table.sql
+++ tools/postgres/migrations/20251112161521_create_savings_table.sql
@@ -2,7 +2,8 @@
 -- +goose StatementBegin
 CREATE TABLE savings (
     id BIGSERIAL PRIMARY KEY,
-    user_id BIGINT NOT NULL REFERENCES users (id),
+    user_id BIGINT NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+,
     amount NUMERIC(10, 2) NOT NULL,
     comment VARCHAR(255),
     saved_at DATE NOT NULL,
