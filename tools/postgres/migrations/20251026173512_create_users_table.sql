b763d8bee86c1174438b33d97111c3550267188d
木村龍太 <kimuraryuuta@kimuraryuutanoMacBook-Air.local>
2025-11-13 02:29:36 +0900

    fix: modify migration definition

--- tools/postgres/migrations/20251026173512_create_users_table.sql
+++ tools/postgres/migrations/20251026173512_create_users_table.sql
@@ -9,18 +9,6 @@ CREATE TABLE users (
     updated_at TIMESTAMP DEFAULT NOW()
 );
 
-CREATE OR REPLACE FUNCTION update_updated_at_column()
-RETURNS TRIGGER AS $$
-BEGIN
-    NEW.updated_at = NOW();
-    RETURN NEW;
-END;
-$$ language 'plpgsql';
-
-CREATE TRIGGER update_users_updated_at
-BEFORE UPDATE ON users
-FOR EACH ROW
-EXECUTE FUNCTION update_updated_at_column();
 -- +goose StatementEnd
 
 -- +goose Down
