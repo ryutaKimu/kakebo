25b7def9d42cde6a8e292aff241e4547b214a468
木村龍太 <kimuraryuuta@kimuraryuutanoMacBook-Air.local>
2025-11-14 00:58:23 +0900

    change amount to target_amount

--- tools/postgres/migrations/20251112153902_create_wants_table.sql
+++ tools/postgres/migrations/20251112153902_create_wants_table.sql
@@ -4,7 +4,7 @@ CREATE TABLE wants (
     id BIGSERIAL PRIMARY KEY,
     user_id BIGINT NOT NULL REFERENCES users (id) ON DELETE CASCADE,
     name VARCHAR(255) NOT NULL, -- 欲しいものの名前
-    amount NUMERIC(10, 2) NOT NULL, -- 目標金額
+    target_amount NUMERIC(10, 2) NOT NULL, -- 目標金額
     target_date DATE NOT NULL, -- 目標購入日
     purchased BOOLEAN DEFAULT FALSE, -- 購入済みフラグ
     purchased_at DATE NULL, -- 購入日
