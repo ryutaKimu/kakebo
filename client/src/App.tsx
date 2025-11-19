743393eb31559c7509c24d26c88c8741b764ab62
木村龍太 <kimuraryuuta@kimuraryuutanoMacBook-Air.local>
2025-11-19 18:14:41 +0900

    modify api

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,21 @@
-import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './pages/home'
-import SignupPage from './pages/signup'
-import LoginPage from './pages/login'
+import "@/App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "@/pages/home";
+import SignupPage from "@/pages/signup";
+import LoginPage from "@/pages/login";
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="signup" element={< SignupPage/>} />
-        <Route path='login' element={<LoginPage/>} />
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="signup" element={<SignupPage />} />
+          <Route path="login" element={<LoginPage />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
